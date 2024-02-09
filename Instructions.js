341a9302bc2a3859eadeb159c0f8fc759f454efe
Fitzroy Nembhard <fitsie007@gmail.com>
2024-02-09 07:16:50 -0500

    Upload of src files for the todolist

---
+++ Instructions.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default class Instructions extends Component {
+    render() { return(
+        <p>Click on an emoji to view the emoji short name.</p>
+    )
+    }
+}
